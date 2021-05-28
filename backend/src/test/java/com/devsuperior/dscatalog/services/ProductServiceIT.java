fc0278c6dc236b92c62f17e7f9e8ad4657ea2f89
Gabriela Gama <gabycgama@hotmail.com>
2021-05-28 20:59:39 -0300

    Integration test

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 
 	@Autowired
@@ -34,12 +37,29 @@ public class ProductServiceIT {
 
 	@Test
 	public void findAllPagedShouldReturnPageWhenParansIsOk() {
-		PageRequest pageRequest = PageRequest.of(0, 10);	
-		Page<ProductDTO> result =  service.findAllPaged(pageRequest);
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
 		Assertions.assertFalse(result.isEmpty());
 		Assertions.assertEquals(0, result.getNumber());
 		Assertions.assertEquals(10, result.getSize());
 		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+	}
+
+	@Test
+	public void findAllPagedShouldReturnEmptyWhenPageDoesNotExist() {
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		Assertions.assertTrue(result.isEmpty());
+	}
+
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortedByName() {
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
 	}
 
 	@Test
