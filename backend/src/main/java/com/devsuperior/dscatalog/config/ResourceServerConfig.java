ed0e973bf68d0b46105fb20dec2e81f4239ee3d4
Gabriela Gama <gabycgama@hotmail.com>
2021-06-02 19:46:22 -0300

    H2 security config

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,7 +16,10 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer // configura a classexom o necessário para ser o ResourceServer do OAuth2
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	private static final String[] PUBLIC = { "/oauth/token" };
+	@Autowired
+	private Environment env;
+	
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 	
@@ -30,6 +36,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		// Libera acesso no H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
 		// determina quem pode acessar o que, através das requisições
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
