6316fc6ec2300e853a483b76acc36b08a9390edf
Gabriela Gama <gabycgama@hotmail.com>
2021-07-15 00:14:34 -0300

    login auth

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import './styles.css';
+import { requestBackendLogin } from 'util/requests';
 
 type FormData = {
   username: string;
@@ -11,7 +12,13 @@ type FormData = {
 const Login = () => {
   const { register, handleSubmit } = useForm<FormData>();
   const onSubmit = (formData: FormData) => {
-    console.log(formData);
+    requestBackendLogin(formData)
+      .then((response) => {
+        console.log('Sucesso', response);
+      })
+      .catch((error) => {
+        console.log('Erro', error);
+      });
   };
 
   return (
