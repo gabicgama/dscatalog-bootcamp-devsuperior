8ba508161b8474c708dc804ca8c3cd8a0f4a3c59
Gabriela Gama <gabycgama@hotmail.com>
2021-07-16 20:41:19 -0300

    hasAnyRole

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -116,3 +116,17 @@ export const isAuthenticated = (): boolean => {
   const tokenData = getTokenData();
   return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
 };
+
+export const hasAnyRole = (roles: Role[]): boolean => {
+  if (roles.length === 0) {
+    return true;
+  }
+
+  const tokenData = getTokenData();
+
+  if (tokenData !== undefined) {
+    return roles.some((role) => tokenData.authorities.includes(role));
+  }
+
+  return false;
+};
