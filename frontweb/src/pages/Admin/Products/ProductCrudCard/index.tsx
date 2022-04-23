c3a2b91efa4e962c19368962c8ed07c34d0eaec1
gabriela <gabycgama@hotmail.com>
2022-04-23 12:20:27 -0300

    ProducCrudCard responsive

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -1,7 +1,7 @@
 import './styles.css';
 import ProductPrice from 'components/ProductPrice';
 import { Product } from 'types/product';
-import CategoryBadge from '../CategoryBadge/indext';
+import CategoryBadge from '../CategoryBadge';
 
 type Props = {
   product: Product;
@@ -13,7 +13,7 @@ const ProductCrudCard = ({ product }: Props) => {
       <div className="product-crud-card-top-container">
         <img src={product.imgUrl} alt={product.name} />
       </div>
-      <div>
+      <div className="product-crud-card-description">
         <div className="product-crud-card-bottom-container">
           <h6>{product.name}</h6>
           <ProductPrice price={product.price} />
@@ -24,7 +24,7 @@ const ProductCrudCard = ({ product }: Props) => {
           ))}
         </div>
       </div>
-      <div className="product-crud-buttons-container">
+      <div className="product-crud-card-buttons-container">
         <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
           EXCLUIR
         </button>
