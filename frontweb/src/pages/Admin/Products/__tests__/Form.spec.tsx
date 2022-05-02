12680b715fa5120bd25eb6afff752380f0e24300
gabriela <gabycgama@hotmail.com>
2022-05-02 13:58:51 -0300

    should clear validation messages when filling out the form correctly

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -37,6 +37,7 @@ describe('Product form create tests', () => {
     const categoriesInput = screen.getByLabelText('Categorias');
 
     const submitButton = screen.getByRole('button', { name: /salvar/i });
+
     await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
     userEvent.type(nameInput, 'Computador');
     userEvent.type(priceInput, '4999,99');
@@ -68,6 +69,42 @@ describe('Product form create tests', () => {
     await waitFor(() => {
       const messages = screen.getAllByText('Campo obrigatório');
       expect(messages).toHaveLength(5);
+    });
+  });
+
+  test('should clear validation messages when filling out the form correctly', async () => {
+    render(
+      <Router history={history}>
+        <Form />
+      </Router>
+    );
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const messages = screen.getAllByText('Campo obrigatório');
+      expect(messages).toHaveLength(5);
+    });
+
+    const nameInput = screen.getByTestId('name');
+    const priceInput = screen.getByTestId('price');
+    const imgUrlInput = screen.getByTestId('imgUrl');
+    const descriptionInput = screen.getByTestId('description');
+    const categoriesInput = screen.getByLabelText('Categorias');
+
+    await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+    userEvent.type(nameInput, 'Computador');
+    userEvent.type(priceInput, '4999,99');
+    userEvent.type(
+      imgUrlInput,
+      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg'
+    );
+    userEvent.type(descriptionInput, 'Computador top');
+
+    await waitFor(() => {
+      const messages = screen.queryAllByText('Campo obrigatório');
+      expect(messages).toHaveLength(0);
     });
   });
 });
